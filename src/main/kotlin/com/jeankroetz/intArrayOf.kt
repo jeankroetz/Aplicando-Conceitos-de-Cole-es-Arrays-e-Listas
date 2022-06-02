25f64293dbc27f8617546e496b63120c27cb0871
jeankroetz <jekroetz@gmail.com>
2022-06-02 14:33:22 -0300

    Introduzido o IntArrayOf. Inicializa o array sem precisar passar a capacidade inicial

---
+++ src/main/kotlin/com/jeankroetz/intArrayOf.kt
@@ -0,0 +1,9 @@
+package com.jeankroetz
+
+//Funciona igual ao IntArray, porém não é necessario passar a capicidade inicial do array. Pode declarar os valores direto.
+
+fun main() {
+    val values = intArrayOf(12,13,24,53,26,75,43)
+
+    println(values.size) //Mostra o numero de elementos do array.
+}
